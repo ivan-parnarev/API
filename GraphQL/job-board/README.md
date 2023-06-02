34b37f2233ff239fe71a4dfe669ceab5df545533
Ivan Parnarev <i.parnarev@gmail.com>
2023-06-02 16:27:25 +0300

    Remove submodule.

---
+++ GraphQL/job-board/README.md
@@ -0,0 +1,19 @@
+# GraphQL - Job Board
+
+It uses Apollo Server with Express, and GraphQL-Request and Apollo Client as GraphQL clients. The application is used to explain queries, mutations, custom object types, authentication, etc.
+
+Application UI:
+http://localhost:3000/
+
+Test queries:
+http://localhost:9000/graphql
+
+Quickstart:
+
+1. Clone repository.
+2. Install dependencies - 'npm install' in 'client/src' and 'server' directories.
+3. Run the server and client:
+
+   - /GraphQL/job-board/server -> node server.js
+
+   - /GraphQL/job-board/client/src -> npm start
---
+++ GraphQL/job-board/client/src/lib/graphql/queries.js
@@ -0,0 +1,105 @@
+import {
+  ApolloClient,
+  ApolloLink,
+  concat,
+  createHttpLink,
+  gql,
+  InMemoryCache,
+} from "@apollo/client";
+import { getAccessToken } from "../auth";
+
+const httpLink = createHttpLink({ uri: "http://localhost:9000/graphql" });
+
+const authLink = new ApolloLink((operation, forward) => {
+  const accessToken = getAccessToken();
+  if (accessToken) {
+    operation.setContext({
+      headers: { 'Authorization': `Bearer ${accessToken}` },
+    });
+  }
+  return forward(operation);
+});
+
+export const apolloClient = new ApolloClient({
+  link: concat(authLink, httpLink),
+  cache: new InMemoryCache(),
+});
+
+export const companyByIdQuery = gql`
+  query CompanyById($id: ID!) {
+    company(id: $id) {
+      id
+      name
+      description
+      jobs {
+        id
+        date
+        title
+      }
+    }
+  }
+`;
+
+const jobDetailFragment = gql`
+  fragment JobDetail on Job {
+    id
+    date
+    title
+    company {
+      id
+      name
+    }
+    description
+  }
+`;
+
+export const jobByIdQuery = gql`
+  query JobById($id: ID!) {
+    job(id: $id) {
+      ...JobDetail
+    }
+  }
+  ${jobDetailFragment}
+`;
+
+export const jobsQuery = gql`
+  query Jobs {
+    jobs {
+      id
+      date
+      title
+      company {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const createJobMutation = gql`
+  mutation CreateJob($input: CreateJobInput!) {
+    job: createJob(input: $input) {
+      ...JobDetail
+    }
+  }
+  ${jobDetailFragment}
+`;
+
+export async function getCompany(id) {
+  const query = gql`
+    query CompanyById($id: ID!) {
+      company(id: $id) {
+        id
+        name
+        description
+        jobs {
+          id
+          date
+          title
+        }
+      }
+    }
+  `;
+  const { data } = await apolloClient.query({ query, variables: { id } });
+  return data.company;
+}
